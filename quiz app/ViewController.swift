fe2fddcd02869d93a4c614c50ddfdb1fa1fefa9c
Mac <mac@macs-macbook-pro-2.local>
2019-01-01 23:42:46 +0530

    Initial Commit

---
+++ quiz app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  quiz app
+//
+//  Created by Mac on 01/01/19.
+//  Copyright © 2019 Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
